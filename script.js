60eebc2bcd70261bfaa2497fd3aa67ce1ffb91ee
aurapetals420 <airapetals420@github.com>
2026-09-16 11:56:34 +0300

    new professional homepage

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function pay(amount){
 function closePay(){
  document.getElementById('popup').classList.remove('show');
 }
+function openImg(src){
+ document.getElementById('bigimg').src=src;
+ document.getElementById('imgview').classList.add('show');
+}
+function closeImg(){
+ document.getElementById('imgview').classList.remove('show');
+}
+function sendBooking(){
+ let name=document.getElementById('bname').value;
+ let phone=document.getElementById('bphone').value;
+ let service=document.getElementById('bservice').value;
+ let date=document.getElementById('bdate').value;
+ let time=document.getElementById('btime').value;
+ let msg=`Hello Aura Petals! I want to book:%0AName: ${name}%0APhone: ${phone}%0AService: ${service}%0ADate: ${date}%0ATime: ${time}`;
+ window.open(`https://wa.me/254716397341?text=${msg}`,'_blank');
+}
